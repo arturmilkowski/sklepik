903dd03c514f8d756a1c7af1b7205d073248bd76
Artur Miłkowski <artur-milkowski@tlen.pl>
2024-11-26 16:33:01 +0100

    Dodanie pliku main.css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,12 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'Sklepik' }}</title>
+    @vite('resources/css/main.css')
   </head>
   <body>
     <x-logo />
     <x-nav />
     {{ $slot }}
-    <hr>
     <x-footer />
   </body>
 </html>
