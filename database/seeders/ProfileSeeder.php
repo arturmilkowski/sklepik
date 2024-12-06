38e1ca02d77be6a7c83fd0828b803be2d0866ece
Artur Miłkowski <artur-milkowski@tlen.pl>
2024-12-06 16:57:06 +0100

    Dodanie modelu user profile

---
+++ database/seeders/ProfileSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProfileSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('profiles')->insert([
+            'user_id' => 1,
+            'voivodeship_id' => 4,
+            'surname' => 'miłkowski',
+            'street' => 'Bałandy 4B/8',
+            'zip_code' => '32-600',
+            'city' => 'Oświęcim',
+            'phone' => '123456789',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
---
+++ tests/Unit/User/ProfileTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Unit\User;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use App\Models\User\{Profile, DeliveryAddress};
+use App\Models\Geo\Voivodeship;
+
+class ProfileTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testMakeProfile(): void
+    {
+        $profile = Profile::factory()->make();
+
+        $this->assertInstanceOf(Profile::class, $profile);
+    }
+
+    public function testCreateProfile(): void
+    {
+        $user = User::factory()->create();
+        $voivodeship = Voivodeship::factory()->create();
+        $profile = Profile::factory()
+            ->for($user)
+            ->for($voivodeship)
+            ->create();
+
+        $this->assertModelExists($profile);
+        $this->assertDatabaseHas('profiles', [
+            'user_id' => $user->id,
+            'voivodeship_id' => $voivodeship->id,
+            'surname' => $profile->surname,
+            'street' => $profile->street,
+            'zip_code' => $profile->zip_code,
+            'city' => $profile->city,
+            'phone' => $profile->phone,
+        ]);
+    }
+
+    public function testProfileBelongsToUser(): void
+    {
+        $profile = Profile::factory()
+            ->for(User::factory())
+            ->for(Voivodeship::factory())
+            ->create();
+
+        $this->assertInstanceOf(User::class, $profile->user);
+    }
+
+    public function testProfileBelongsToVoivodeship(): void
+    {
+        $profile = Profile::factory()
+            ->for(User::factory())
+            ->for(Voivodeship::factory())
+            ->create();
+
+        $this->assertInstanceOf(Voivodeship::class, $profile->voivodeship);
+    }
+
+    /*
+    public function testProfileHasOneDeliveryAddress(): void
+    {
+        $profile = Profile::factory()
+            ->for(User::factory())
+            ->for(Voivodeship::factory())
+            ->has(DeliveryAddress::factory()->for(Voivodeship::factory()))
+            ->create();
+
+        $deliveryAddress = $profile->deliveryAddress;
+
+        $this->assertInstanceOf(DeliveryAddress::class, $deliveryAddress);
+    }
+        */
+}
